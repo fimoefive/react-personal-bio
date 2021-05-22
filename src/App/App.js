e18ac67d0c8a5c994831fc56db48532cbede4c62
fimoefive <brothermj@hotmail.com>
2021-05-22 11:13:03 -0500

    added folders view, components, forms, and sample data. I also created projectData.js, Projects.js, SingleProject.js, ProjectForm.js Home.js, NavBar.js, ProjectCard.js, Routes.js, auth.js, and SingleProjectCard.js.

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import 'firebase/auth';
+import NavBar from '../components/NavBar';
+import Routes from '../helpers/Routes';
+import './App.scss';
+
+function App() {
+  const handleClick = (e) => {
+  };
+
+  return (
+    <div className='App'>
+
+    </div>
+  );
+}
+
+export default App;
