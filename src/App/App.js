5ad2accd260cb7f21080e542f6cd862898f044ec
fimoefive <brothermj@hotmail.com>
2021-05-22 12:21:10 -0500

    added added auth useEffect in App.js file.

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
 import 'firebase/auth';
+import { useHistory } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
 import './App.scss';
 
 function App() {
-  const handleClick = (e) => {
-  };
+  const [admin, setAdmin] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed && (authed.uid === process.env.REACT_APP_ADMIN_UID)) {
+        setAdmin(true);
+      } else if (admin || admin === null) {
+        setAdmin(false);
+        setLoggedInUser(false);
+      }
+    });
+  }, []);
 
   return (
     <div className='App'>
