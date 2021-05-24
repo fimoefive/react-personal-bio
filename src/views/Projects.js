bde0413dddbea4142175fa54c5d2dfcd46e6319d
fimoefive <brothermj@hotmail.com>
2021-05-24 18:52:50 -0500

    wrote the Projects function for the Projects.js file in the views folder. Also imported the ProjectForm and Card into the Projects.js file.:

---
+++ src/views/Projects.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
+import ProjectCard from '../components/ProjectCard';
+import ProjectForm from '../forms/ProjectForm';
+// import './viewStyles/projects.scss';
+
+function Projects({
+  user, projects, setProjects
+}) {
+  const [showAddProject, setAddProject] = useState(false);
+
+  const handleClick = () => {
+    setAddProject((prevState) => !prevState);
+  };
+
+  return (
+    <div className="proView">
+      <div className="card-container">
+        <div>
+          {!showAddProject
+            ? <Button className="addPrjBtn" onClick={handleClick}></Button>
+            : <div>
+              <Button className="closeForm" onClick={handleClick}><img alt="close button" /></Button>
+              <ProjectForm
+                setProjects={setProjects}
+                user={user}
+              />
+            </div>
+          }
+        </div>
+        {projects.length && projects.map((proInfo) => (
+          <ProjectCard className="proCard"
+            key={proInfo.firebaseKey}
+            firebaseKey={proInfo.firebaseKey}
+            organizationName={proInfo.organizationName}
+            setProjects={setProjects}
+            uid={proInfo.uid}
+            user={user}
+          />
+        ))};
+        </div>
+    </div>
+  );
+}
+
+Projects.propTypes = {
+  projects: PropTypes.array.isRequired,
+  setProjects: PropTypes.func.isRequired,
+  user: PropTypes.any
+};
+
+export default Projects;
