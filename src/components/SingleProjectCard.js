81f14408d7a5848f9cc5d0a31920c64e1432c9f4
fimoefive <brothermj@hotmail.com>
2021-05-28 22:28:25 -0500

    added view Button and useHistory in the ProjectCard function and file. Also added a case for history.push for the switch case in handleClick function and declared history for useHistory on line 23.

--- src/components/SingleProjectCard.js
+++ src/components/SingleProjectCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function SingleProjectCard({ children, project }) {
   return (
     <div>
-      <h2>Project: {project.name}</h2>
+      <h2>Project: {project.projectName}</h2>
       {children}
       <footer>&#169; 2021</footer>
     </div>
